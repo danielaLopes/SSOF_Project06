64056fcc1aa11474afeb930d2acb6f6527367a99
Antonio Vieira <amsmv11@gmail.com>
2019-11-20 10:49:02 +0000

    faz merge dos patterns iguais

--- pattern.py
+++ pattern.py
@@ -16,8 +16,23 @@ def parse(json_pattern_file):
 
 	for json_pattern in parsed_json_pattern_list:
 		#print(json_pattern)
-		pattern = Pattern(json_pattern['vulnerability'],json_pattern['sources'],json_pattern['sanitizers'],json_pattern['sinks'])
-		#print(pattern)
-		pattern_list.append(pattern)
+		add = True
 		
+		#print(pattern)
+		if(len(pattern_list) != 0):
+			for e in pattern_list:
+				if(json_pattern['vulnerability'] == e.vulnerability):
+					e.sources = e.sources + list(set(json_pattern['sources']) - set(e.sources))
+					e.sanitizers = e.sanitizers + list(set(json_pattern['sanitizers']) - set(e.sanitizers))
+					e.sinks = e.sinks + list(set(json_pattern['sinks']) - set(e.sinks))
+					add = False
+		if(add):	
+			pattern = Pattern(json_pattern['vulnerability'],json_pattern['sources'],json_pattern['sanitizers'],json_pattern['sinks'])
+			pattern_list.append(pattern)
+		
+	for e in pattern_list:
+		print(e.vulnerability)
+		print(e.sources)
+		print(e.sanitizers)
+		print(e.sinks)
 	return pattern_list
