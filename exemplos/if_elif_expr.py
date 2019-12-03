515290b3490115f84e9ddc0e6d0a7c1770d489a0
root <kevin-batistac@hotmail.com>
2019-12-03 23:51:36 +0000

    Tests

--- exemplos/if_elif_expr.py
+++ exemplos/if_elif_expr.py
@@ -1,9 +1,10 @@
-a = 4
 if a > 2:
-    a+2
+    a= a+2
+    a = ContactMailForm(a)
     print("ola")
 elif a > 3:
-    not a
+    b =not a
 else:
-    not b
+    a= not b
+    a = Response(a)
     a +3
---
+++ testes/T06-04/program.py
@@ -0,0 +1,12 @@
+if a:
+    x = "ola"
+    if b:
+        y = adeus
+        if c:
+        	x = get(k)
+elif b:
+    z = adeus
+else:
+	if lol:
+		w = adeus
+		x = RawSQL(x)
